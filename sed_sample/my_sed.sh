664a49c3315c5b8aad7825ae8edec582d524508c
levin <3259963@qq.com>
2017-05-03 21:46:50 +0800

    add my_sed.sh

--- sed_sample/my_sed.sh
+++ sed_sample/my_sed.sh
@@ -1,20 +1,8 @@
 #! /bin/bash
 
-#IFS=$'\r\n'
-#for line in `cat t1.txt`
-#do
-    #echo ${line}
-#    cust_code=`echo ${line} | awk -F '\t' '{print $1}'`
-#    prod_code=`echo ${line} | awk -F '\t' '{print $2}'`
-#    orig_city=`echo ${line} | awk -F '\t' '{print $3}'`
-#    start_dt=`echo ${line} | awk -F '\t' '{print $4}'`
-#    end_dt=`echo ${line} | awk -F '\t' '{print $5}'`
-#    echo "$cust_code--$prod_code--$end_dt" 
-#done
-
 para_file=./para.txt
 template_file=./template.sql
-IFS=$'\t\r\n'
+IFS=$'\t'
 while read -r customer_code prod_code orig_city start_dt end_dt
 do
     sed -e "s|\[\[\[start_dt\]\]\]|${start_dt}|g" \
